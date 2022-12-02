bf94a044b7c9f47ffc438e0f4384be5fbe326ed3
Stuart Carnie <stuart.carnie@gmail.com>
2022-12-02 11:47:58 +1100

    chore: Add comments

--- rust/2022/src/bin/day01b.rs
+++ rust/2022/src/bin/day01b.rs
@@ -4,11 +4,16 @@ use input::day01::INPUT;
 fn main() {
     let sums = INPUT
         .lines()
+        // create groups, breaking each time an empty string is observed
         .group_by(|v| { *v != "" })
         .into_iter()
+        // convert all the strings to i32s, skipping any that fail to parse (i.e. empty strings)
         .map(|(_, g)| { g.filter_map(|s| s.parse::<i32>().ok()).sum::<i32>() })
+        // sort the integers
         .sorted()
+        // reverse their order
         .rev()
+        // collect them into a vector
         .collect::<Vec<_>>();
     println!("{}", sums[0..3].into_iter().sum::<i32>());
 }
